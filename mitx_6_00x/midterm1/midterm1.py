5d443d2e3e85059fd69068aa46a1435a91f9da8d
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2012-11-02 16:27:48 +0100

    mitx midderm: more stuff

--- mitx_6_00x/midterm1/midterm1.py
+++ mitx_6_00x/midterm1/midterm1.py
@@ -9,9 +9,59 @@ def laceStrings(s1, s2):
     beginning with s1. If strings are not of same length,
     then the extra elements should appear at the end.
     """
-    # Your Code Here
     return "".join([
         element for element in itertools.chain.from_iterable(
                 itertools.izip_longest(s1, s2, fillvalue=None))
         if element
     ])
+
+
+def laceStringsRecur(s1, s2):
+    """
+    s1 and s2 are strings.
+
+    Returns a new str with elements of s1 and s2 interlaced,
+    beginning with s1. If strings are not of same length,
+    then the extra elements should appear at the end.
+    """
+    def helpLaceStrings(s1, s2, out):
+        if s1 == '':
+            return out + s2
+        if s2 == '':
+            return out + s1
+        else:
+            return helpLaceStrings(s1[1:], s2[1:], out + s1[0] + s2[0])
+    return helpLaceStrings(s1, s2, '')
+
+
+def fixedPoint(f, epsilon):
+    """
+    f: a function of one argument that returns a float
+    epsilon: a small float
+
+    returns the best guess when that guess is less than epsilon
+    away from f(guess) or after 100 trials, whichever comes first.
+    """
+    guess = 1.0
+    for i in range(100):
+        if abs(f(guess) - guess) <= epsilon:
+            return guess
+        else:
+            guess = f(guess)
+    return guess
+
+
+def sqrt_ver1(a):
+    def tryit(x):
+        return 0.5 * (a / x + x)
+    return fixedPoint(tryit(a), 0.0001)
+
+
+def babylon(a):
+    def test(x):
+        return 0.5 * ((a / x) + x)
+    return test
+
+
+def sqrt_ver2(a):
+    return fixedPoint(babylon(a), 0.0001)
--- mitx_6_00x/midterm1/tests.py
+++ mitx_6_00x/midterm1/tests.py
@@ -7,3 +7,23 @@ def test_lace_strings():
     print result
     print expected
     assert result == expected
+
+
+def test_lace_strings_recur():
+    from midterm1 import laceStringsRecur
+    result = laceStringsRecur('abcd', 'efghi')
+    expected = 'aebfcgdhi'
+    print result
+    print expected
+    assert result == expected
+
+
+def test_fixed_point():
+    from midterm1 import fixedPoint, sqrt_ver2
+    epsilon = 0.0001
+    r1 = fixedPoint(lambda x: 0.5 * (x ** 2), epsilon)
+    r2 = sqrt_ver2(64)
+    print "r1", r1
+    print "r2", r2
+    assert r1 == 0
+    assert r2 - 8 < epsilon
