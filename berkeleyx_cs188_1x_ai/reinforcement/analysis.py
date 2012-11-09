2ed0e172e0a54b4cbc877d9c293813fe199679b8
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2012-11-09 15:38:38 +0100

    ai: q-learning question

--- berkeleyx_cs188_1x_ai/reinforcement/analysis.py
+++ berkeleyx_cs188_1x_ai/reinforcement/analysis.py
@@ -63,8 +63,7 @@ def question3e():
 def question6():
     answerEpsilon = None
     answerLearningRate = None
-    return answerEpsilon, answerLearningRate
-    # If not possible, return 'NOT POSSIBLE'
+    return 'NOT POSSIBLE'
 
 
 if __name__ == '__main__':
