3a9b036a8894f1dcbb0a75d73352cb9f5650ad11
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2012-12-09 22:48:34 +0100

    cuda: fixes

--- coursera_cuda/mp1.c
+++ coursera_cuda/mp1.c
@@ -1,12 +1,11 @@
 // MP 1
 #include <wb.h>
-#include <cuda.h>
 
 __global__ void vecAdd(float * in1, float * in2, float * out, int len) {
     //@@ Insert code to implement vector addition here
     int i = threadIdx.x + blockDim.x * blockIdx.x;
-    if (i < n) {
-        C[i] = A[i] + B[i];
+    if (i < len) {
+        out[i] = in1[i] + in2[i];
     }
 }
 
@@ -32,16 +31,16 @@ int main(int argc, char ** argv) {
 
 	wbTime_start(GPU, "Allocating GPU memory.");
     //@@ Allocate GPU memory here
-    CUDA_SAFE_CALL( cudaMalloc( (void **)&deviceInput1, sizeof(float) * inputLength) );
-    CUDA_SAFE_CALL( cudaMalloc( (void **)&deviceInput2, sizeof(float) * inputLength) );
-    CUDA_SAFE_CALL( cudaMalloc( (void **)&deviceOutput, sizeof(float) * inputLength) );
+    cudaMalloc( (void **)&deviceInput1, sizeof(float) * inputLength);
+    cudaMalloc( (void **)&deviceInput2, sizeof(float) * inputLength);
+    cudaMalloc( (void **)&deviceOutput, sizeof(float) * inputLength);
 
     wbTime_stop(GPU, "Allocating GPU memory.");
 
     wbTime_start(GPU, "Copying input memory to the GPU.");
     //@@ Copy memory to the GPU here
-    CUDA_SAFE_CALL( cudaMemcpy(deviceInput1, hostInput1, sizeof(float) * inputLength, cudaMemcpyHostToDevice) );
-    CUDA_SAFE_CALL( cudaMemcpy(deviceInput2, hostInput2, sizeof(float) * inputLength, cudaMemcpyHostToDevice) );
+    cudaMemcpy(deviceInput1, hostInput1, sizeof(float) * inputLength, cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceInput2, hostInput2, sizeof(float) * inputLength, cudaMemcpyHostToDevice);
 
     wbTime_stop(GPU, "Copying input memory to the GPU.");
     
@@ -52,22 +51,22 @@ int main(int argc, char ** argv) {
     
     wbTime_start(Compute, "Performing CUDA computation");
     //@@ Launch the GPU Kernel here
-    CUDA_SAFE_CALL(vecAdd<<<DimGrid,DimBlock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength));
+    vecAdd<<<DimGrid,DimBlock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
 
     cudaThreadSynchronize();
     wbTime_stop(Compute, "Performing CUDA computation");
     
     wbTime_start(Copy, "Copying output memory to the CPU");
     //@@ Copy the GPU memory back to the CPU here
-    CUDA_SAFE_CALL( cudaMemcpy(hostOutput, deviceOutput, sizeof(float) * inputLength, cudaMemcpyDeviceToHost) );
+    cudaMemcpy(hostOutput, deviceOutput, sizeof(float) * inputLength, cudaMemcpyDeviceToHost);
 
     wbTime_stop(Copy, "Copying output memory to the CPU");
 
     wbTime_start(GPU, "Freeing GPU Memory");
     //@@ Free the GPU memory here
-    CUDA_SAFE_CALL(cudaFree(deviceInput1));
-    CUDA_SAFE_CALL(cudaFree(deviceInput2));
-    CUDA_SAFE_CALL(cudaFree(deviceOutput));
+    cudaFree(deviceInput1);
+    cudaFree(deviceInput2);
+    cudaFree(deviceOutput);
 
     wbTime_stop(GPU, "Freeing GPU Memory");
 
@@ -79,5 +78,3 @@ int main(int argc, char ** argv) {
 
     return 0;
 }
-
-
