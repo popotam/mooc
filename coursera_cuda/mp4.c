3f293e82ad5da78615424ad63482c2da77728540
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2013-01-15 23:57:40 +0100

    cuda: first reduction

---
+++ coursera_cuda/mp4.c
@@ -0,0 +1,116 @@
+// MP 4 Reduction
+// Given a list (lst) of length n
+// Output its sum = lst[0] + lst[1] + ... + lst[n-1];
+// Due Tuesday, January 15, 2013 at 11:59 p.m. PST
+
+#include <wb.h>
+
+#define BLOCK_SIZE 512 //@@ You can change this
+
+#define wbCheck(stmt) do {                                 \
+        cudaError_t err = stmt;                            \
+        if (err != cudaSuccess) {                          \
+            wbLog(ERROR, "Failed to run stmt ", #stmt);    \
+            return -1;                                     \
+        }                                                  \
+    } while(0)
+
+__global__ void total(float * input, float * output, int len) {
+    //@@ Insert code to implement vector addition here
+	__shared__ float partialSum[2 * BLOCK_SIZE];
+
+    unsigned int t = threadIdx.x + blockDim.x * blockIdx.x;
+    unsigned int start = 2 * blockIdx.x * blockDim.x;
+
+    // load to shared
+    partialSum[t] = (start + t < len) ? input[start + t] : 0;
+    partialSum[blockDim.x + t] = (start + blockDim.x + t) ? input[start + blockDim.x + t] : 0;
+
+    // reduce
+    for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+    	__syncthreads();
+    	if (t < stride) {
+    		partialSum[t] += partialSum[stride + t];
+    	}
+    }
+    __syncthreads();
+    // write to output
+    if (t == 0) {
+    	output[blockIdx.x] = partialSum[0];
+    }
+}
+
+int main(int argc, char ** argv) {
+    int ii;
+    wbArg_t args;
+    float * hostInput; // The input 1D list
+    float * hostOutput; // The output list
+    float * deviceInput;
+    float * deviceOutput;
+    int numInputElements; // number of elements in the input list
+    int numOutputElements; // number of elements in the output list
+
+    args = wbArg_read(argc, argv);
+
+    wbTime_start(Generic, "Importing data and creating memory on host");
+    hostInput = (float *) wbImport(wbArg_getInputFile(args, 0), &numInputElements);
+    numOutputElements = numInputElements / (BLOCK_SIZE<<1);
+    if (numInputElements % (BLOCK_SIZE<<1)) {
+        numOutputElements++;
+    }
+    hostOutput = (float*) malloc(numOutputElements * sizeof(float));
+    wbTime_stop(Generic, "Importing data and creating memory on host");
+
+    wbLog(TRACE, "The number of input elements in the input is ", numInputElements);
+    wbLog(TRACE, "The number of output elements in the input is ", numOutputElements);
+
+    wbTime_start(GPU, "Allocating GPU memory.");
+    //@@ Allocate GPU memory here
+    wbCheck( cudaMalloc( (void **)&deviceInput, sizeof(float) * numInputElements) );
+    wbCheck( cudaMalloc( (void **)&deviceOutput, sizeof(float) * numOutputElements) );
+    wbTime_stop(GPU, "Allocating GPU memory.");
+
+    wbTime_start(GPU, "Copying input memory to the GPU.");
+    //@@ Copy memory to the GPU here
+    cudaMemcpy(deviceInput, hostInput, sizeof(float) * numInputElements, cudaMemcpyHostToDevice);
+    wbTime_stop(GPU, "Copying input memory to the GPU.");
+    
+    //@@ Initialize the grid and block dimensions here
+    dim3 DimGrid((numInputElements - 1) / BLOCK_SIZE + 1, 1, 1);
+    dim3 DimBlock(BLOCK_SIZE, 1, 1);
+
+    wbTime_start(Compute, "Performing CUDA computation");
+    //@@ Launch the GPU Kernel here
+    total<<<DimGrid,DimBlock>>>(deviceInput, deviceOutput, numInputElements);
+    cudaThreadSynchronize();
+    wbTime_stop(Compute, "Performing CUDA computation");
+    
+    wbTime_start(Copy, "Copying output memory to the CPU");
+    //@@ Copy the GPU memory back to the CPU here
+    cudaMemcpy(hostOutput, deviceOutput, sizeof(float) * numOutputElements, cudaMemcpyDeviceToHost);
+    wbTime_stop(Copy, "Copying output memory to the CPU");
+
+    /********************************************************************
+     * Reduce output vector on the host
+     * NOTE: One could also perform the reduction of the output vector
+     * recursively and support any size input. For simplicity, we do not
+     * require that for this lab.
+     ********************************************************************/
+    for (ii = 1; ii < numOutputElements; ii++) {
+        hostOutput[0] += hostOutput[ii];
+    }
+
+    wbTime_start(GPU, "Freeing GPU Memory");
+    //@@ Free the GPU memory here
+    cudaFree(deviceInput);
+    cudaFree(deviceOutput);
+
+    wbTime_stop(GPU, "Freeing GPU Memory");
+
+    wbSolution(args, hostOutput, 1);
+
+    free(hostInput);
+    free(hostOutput);
+
+    return 0;
+}
