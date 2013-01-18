399cebfb0cf7a3ead17dd41b38dc6d6f8b18f095
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2013-01-18 14:34:12 +0100

    cuda: mp5 final

--- coursera_cuda/mp5.c
+++ coursera_cuda/mp5.c
@@ -15,7 +15,7 @@
         }                                                  \
     } while(0)
 
-__global__ void scan(float * input, float * output, int len) {
+__global__ void scan(float * input, float * output, float * inter, int len, bool save) {
     //@@ Modify the body of this function to complete the functionality of
     //@@ the scan on the device
     //@@ You may need multiple kernel calls; write your kernels before this
@@ -32,6 +32,7 @@ __global__ void scan(float * input, float * output, int len) {
     shared[t] = (start + t < len) ? input[start + t] : 0;
     shared[BLOCK_SIZE + t] = (start + BLOCK_SIZE + t < len) ? input[start + BLOCK_SIZE + t] : 0;
 
+    // reduce
     stride = 1;
     while (stride <= BLOCK_SIZE) {
         __syncthreads();
@@ -42,6 +43,7 @@ __global__ void scan(float * input, float * output, int len) {
         stride *= 2;
     }
 
+    // post reduce
     for (stride = BLOCK_SIZE / 2; stride >= 1; stride /= 2) {
         __syncthreads();
         index = (t + 1) * stride * 2 - 1;
@@ -50,12 +52,32 @@ __global__ void scan(float * input, float * output, int len) {
         }
     }
 
+    // save in output
     __syncthreads();
     if (start + t < len) {
         output[start + t] = shared[t];
     }
     if (start + BLOCK_SIZE + t < len) {
         output[start + BLOCK_SIZE + t] = shared[BLOCK_SIZE + t];
+    }
+
+    // save to intermediary array
+    if (save && t == 0) {
+        inter[b] = shared[2 * BLOCK_SIZE - 1];
+    }
+}
+
+__global__ void scan_inter(float * inout, float * blockSums, int len) {
+	unsigned int t = threadIdx.x;
+	unsigned int b = blockIdx.x;
+    unsigned int start = 2 * b * BLOCK_SIZE;
+    if (b > 0) {
+		if (start + t < len) {
+			inout[start + t] += blockSums[b - 1];
+		}
+		if (start + BLOCK_SIZE + t < len) {
+			inout[start + BLOCK_SIZE + t] += blockSums[b - 1];
+		}
     }
 }
 
@@ -65,7 +87,10 @@ int main(int argc, char ** argv) {
     float * hostOutput; // The output list
     float * deviceInput;
     float * deviceOutput;
+    float * deviceInterIn;
+    float * deviceInterOut;
     int numElements; // number of elements in the list
+    int numBlocks;
 
     args = wbArg_read(argc, argv);
 
@@ -75,10 +100,13 @@ int main(int argc, char ** argv) {
     wbTime_stop(Generic, "Importing data and creating memory on host");
 
     wbLog(TRACE, "The number of input elements in the input is ", numElements);
+    numBlocks = (numElements - 1) / (2 * BLOCK_SIZE) + 1;
 
     wbTime_start(GPU, "Allocating GPU memory.");
     wbCheck(cudaMalloc((void**)&deviceInput, numElements*sizeof(float)));
     wbCheck(cudaMalloc((void**)&deviceOutput, numElements*sizeof(float)));
+    wbCheck(cudaMalloc((void**)&deviceInterIn, numBlocks*sizeof(float)));
+    wbCheck(cudaMalloc((void**)&deviceInterOut, numBlocks*sizeof(float)));
     wbTime_stop(GPU, "Allocating GPU memory.");
 
     wbTime_start(GPU, "Clearing output memory.");
@@ -90,13 +118,19 @@ int main(int argc, char ** argv) {
     wbTime_stop(GPU, "Copying input memory to the GPU.");
 
     //@@ Initialize the grid and block dimensions here
-    dim3 DimGrid((numElements - 1) / (2 * BLOCK_SIZE) + 1, 1, 1);
+    dim3 DimGrid(numBlocks, 1, 1);
     dim3 DimBlock(BLOCK_SIZE, 1, 1);
 
     wbTime_start(Compute, "Performing CUDA computation");
     //@@ Modify this to complete the functionality of the scan
     //@@ on the deivce
-    scan<<<DimGrid,DimBlock>>>(deviceInput, deviceOutput, numElements);
+    scan<<<DimGrid,DimBlock>>>(deviceInput, deviceOutput, deviceInterIn, numElements, 1);
+    if (numBlocks > 1) {
+        dim3 InterDimGrid(1, 1, 1);
+        dim3 InterDimBlock(BLOCK_SIZE, 1, 1);
+        scan<<<InterDimGrid,InterDimBlock>>>(deviceInterIn, deviceInterOut, 0, numBlocks, 0);
+        scan_inter<<<DimGrid,DimBlock>>>(deviceOutput, deviceInterOut, numElements);
+    }
 
     cudaDeviceSynchronize();
     wbTime_stop(Compute, "Performing CUDA computation");
