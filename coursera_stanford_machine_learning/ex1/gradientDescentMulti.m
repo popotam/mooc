07927c40ab8cb6b71c290ebf8d602fbd53fd8b88
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2013-05-04 01:37:59 +0200

    Machine Learning: ex1 complete

--- coursera_stanford_machine_learning/ex1/gradientDescentMulti.m
+++ coursera_stanford_machine_learning/ex1/gradientDescentMulti.m
@@ -5,31 +5,28 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(X, 2); % number of features
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    delta = zeros(n, 1);
+    for i = 1:n,
+        delta(i) = (1 / m) * sum((X * theta - y) .* X(:, i));
+    end
+    theta = theta - alpha * delta;
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
